a6434412dc057dd18735b4914f3f57435c5b66eb
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2021-02-02 17:11:09 +0100

    Refactoring main.

--- src/main.c
+++ src/main.c
@@ -16,20 +16,4 @@
     return 0;
   }
 
-#else
-
-  #include <stdio.h>
-  #include <glib.h>
-
-  #include "test_vm.h"
-
-  int main(int argc, char** argv) {
-    g_test_init(&argc, &argv, NULL);
-    printf("Testing...");
-
-    g_test_add("/vm/registries_are_zero", vm_fixture, NULL, vm_setup, vm_registries_are_zero, vm_teardown);
-
-    return g_test_run();
-  }
-
 #endif // TEST
